74d045df06bf4b2bcdd495858dc9e3a04ba31a66
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-16 19:51:17 +0200

    small change

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace _30.arrays
             int[] myInt = new int[10];
             int count = 0;
             myInt[3] = 3;
+            myInt[4] = 5;
 
             for (int i = 0; i < myInt.Length; i++)
             {
